fe33085e3a7a63fc7c90104b24b8574bf3847ae9
DeekshaNaik028 <naikdeeksh24912@gmail.com>
2025-04-17 15:43:24 +0530

    Update and rename Test.java to MiniTest.java

--- src/test/java/com/example/MiniTest.java
+++ src/test/java/com/example/MiniTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Test {
+public class MiniTest {
     
     @Test
     public void simpleTest() {
